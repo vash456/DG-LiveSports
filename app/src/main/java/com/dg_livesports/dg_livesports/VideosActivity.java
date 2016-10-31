3346b47ea90d43fbbf4a29bde6089241a30746aa
vash456 <darlin.estrada456@gmail.com>
2016-10-31 01:21:17 -0500

    Version 1.0

---
+++ app/src/main/java/com/dg_livesports/dg_livesports/VideosActivity.java
@@ -0,0 +1,13 @@
+package com.dg_livesports.dg_livesports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videos);
+    }
+}
